94a9a897e99f78595e2d696a4d75f2ec5632a757
jefersonfsilva <jefersonfs@gmail.com>
2019-12-17 10:16:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/br/pandacontrol/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.br.pandacontrol.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.pandacontrol.course.entities.User;
+import com.br.pandacontrol.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

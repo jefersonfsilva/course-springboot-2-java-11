9e1f495798f317ec8e0b6bced3d9a8ede7dd5639
jefersonfsilva <jefersonfs@gmail.com>
2019-12-18 19:20:47 -0300

    Exception handling - findById

--- src/main/java/com/br/pandacontrol/course/services/UserService.java
+++ src/main/java/com/br/pandacontrol/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.br.pandacontrol.course.entities.User;
 import com.br.pandacontrol.course.repositories.UserRepository;
+import com.br.pandacontrol.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
